5d562963eea08cc321d2df54dbd86ee2b5d2f243
asher scott <asher.scott6@gmail.com>
2022-05-18 20:58:56 -0500

    cors test

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,18 +7,26 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:4000'
+    origins '*'
 
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 
-  allow do
-    origins 'https://asher-scott-finance-tracker.herokuapp.com/'
+  # allow do
+  #   origins 'http://localhost:4000'
 
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
-  end
+  #   resource '*',
+  #     headers: :any,
+  #     methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  # end
+
+  # allow do
+  #   origins 'https://asher-scott-finance-tracker.herokuapp.com/'
+
+  #   resource '*',
+  #     headers: :any,
+  #     methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  # end
 end
