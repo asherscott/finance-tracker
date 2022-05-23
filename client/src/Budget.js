5e4e94516b9a42be8a9cae3b931508f3bebcb1e6
asher scott <asher.scott6@gmail.com>
2022-05-23 12:48:50 -0500

    google charts

--- client/src/Budget.js
+++ client/src/Budget.js
@@ -1,14 +1,29 @@
 import { useState } from "react";
 import RenderRows from "./RenderRows";
+import { Chart } from "react-google-charts";
 
 function Budget({ user }) {
   const [budgetEntries, setBudgetEntries] = useState(
     user.journal_entries.filter((entry) => entry.category.name === "Budget")
   );
 
+  const budgetPieData = budgetEntries.map((entry) => [
+    entry.sub_category.name,
+    entry.amount,
+  ]);
+  budgetPieData.unshift(["Category", "Amount"]);
+
   return (
     <div>
       <h2>Budget</h2>
+
+      <Chart
+        chartType="PieChart"
+        width="100%"
+        height="400px"
+        data={budgetPieData}
+        options={{ pieHole: 0.6 }}
+      />
 
       <table>
         <thead>
