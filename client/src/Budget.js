530e5e2febe4100f7a47b1d1a70ef1c520ecebfa
asher scott <asher.scott6@gmail.com>
2022-05-23 19:11:58 -0500

    new categories

--- client/src/Budget.js
+++ client/src/Budget.js
@@ -46,7 +46,6 @@ function Budget({ user }) {
             journalEntries={budgetEntries}
             setJournalEntries={setBudgetEntries}
             user={user}
-            showDropdown={false}
           />
         </table>
       </div>
--- client/src/RenderRows.js
+++ client/src/RenderRows.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-function RenderRows({ journalEntries, setJournalEntries, user, showDropdown }) {
+function RenderRows({ journalEntries, setJournalEntries, user }) {
   const [subCategory, setSubCategory] = useState("");
   const [amount, setAmount] = useState("");
   const [note, setNote] = useState("");
@@ -10,7 +10,10 @@ function RenderRows({ journalEntries, setJournalEntries, user, showDropdown }) {
 
   useEffect(() => {
     setNewCategory(false);
-  }, [edit]);
+    if (newEntry && edit) {
+      setNewEntry(false);
+    }
+  }, [edit, newEntry]);
 
   const subCategories = journalEntries.map((entry) => entry.sub_category.name);
 
@@ -18,16 +21,11 @@ function RenderRows({ journalEntries, setJournalEntries, user, showDropdown }) {
     .filter((category, index) => subCategories.indexOf(category) === index)
     .map((category) => <option key={category}>{category}</option>);
 
-  function showCategoryForm() {
-    setNewCategory(!newCategory);
-    setSubCategory("");
-  }
-
   const renderEntries = journalEntries.map((entry) =>
     edit === entry.id ? (
       <tr key={entry.id}>
         <td>
-          {!newCategory ? (
+          {!newCategory || newEntry ? (
             <select
               type="dropdown"
               onChange={(e) => {
@@ -89,6 +87,81 @@ function RenderRows({ journalEntries, setJournalEntries, user, showDropdown }) {
     )
   );
 
+  function isNewCategory(id) {
+    const config = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        category_id: 3,
+        name: subCategory,
+      }),
+    };
+
+    fetch("/sub_categories", config).then((r) =>
+      r.ok
+        ? r
+            .json()
+            .then((newCat) =>
+              id ? patchEntry(id, newCat.id) : postEntry(newCat.id)
+            )
+        : r.json().then((error) => console.log(error))
+    );
+  }
+
+  function patchEntry(id, newCatId) {
+    const subCategoryId = journalEntries.find(
+      (entry) => entry.sub_category.name === subCategory
+    )
+      ? journalEntries.find((entry) => entry.sub_category.name === subCategory)
+          .sub_category.id
+      : newCatId;
+
+    const config = {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        sub_category_id: subCategoryId,
+        amount,
+        note,
+      }),
+    };
+
+    fetch("/journal_entries/" + id, config).then((r) =>
+      r.ok
+        ? r.json().then((update) => onEdit(update))
+        : r.json().then((error) => console.log(error))
+    );
+  }
+
+  function postEntry(newCatId) {
+    const subCategoryId = journalEntries.find(
+      (entry) => entry.sub_category.name === subCategory
+    )
+      ? journalEntries.find((entry) => entry.sub_category.name === subCategory)
+          .sub_category.id
+      : newCatId;
+
+    const config = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        sub_category_id: subCategoryId,
+        amount,
+        note,
+        user_id: user.id,
+      }),
+    };
+
+    fetch("/journal_entries", config).then((r) =>
+      r.ok
+        ? r.json().then((update) => {
+            onEdit(update);
+            setNewEntry(false);
+          })
+        : r.json().then((error) => console.log(error))
+    );
+  }
+
   function handleDelete(id) {
     fetch("/journal_entries/" + id, {
       method: "DELETE",
@@ -118,46 +191,14 @@ function RenderRows({ journalEntries, setJournalEntries, user, showDropdown }) {
   }
 
   function handleSave(id) {
-    if (id) {
-      const subCategoryId = journalEntries.find(
-        (entry) => entry.sub_category.name === subCategory
-      ).sub_category.id;
-
-      const config = {
-        method: "PATCH",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ sub_category_id: subCategoryId, amount, note }),
-      };
-
-      fetch("/journal_entries/" + id, config).then((r) =>
-        r.ok
-          ? r.json().then((update) => onEdit(update))
-          : r.json().then((error) => console.log(error))
-      );
+    if (!subCategory) {
+      alert("error: select category");
     } else {
-      const subCategoryId = journalEntries.find(
-        (entry) => entry.sub_category.name === subCategory
-      ).sub_category.id;
-
-      const config = {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          sub_category_id: subCategoryId,
-          amount,
-          note,
-          user_id: user.id,
-        }),
-      };
-
-      fetch("/journal_entries", config).then((r) =>
-        r.ok
-          ? r.json().then((update) => {
-              onEdit(update);
-              setNewEntry(false);
-            })
-          : r.json().then((error) => console.log(error))
-      );
+      if (!journalEntries.find((je) => je.sub_category.name === subCategory)) {
+        isNewCategory(id);
+      } else {
+        id ? patchEntry(id) : postEntry();
+      }
     }
   }
 
@@ -167,23 +208,47 @@ function RenderRows({ journalEntries, setJournalEntries, user, showDropdown }) {
       {!newEntry ? (
         <tr>
           <td>
-            <button onClick={() => setNewEntry(true)}>+</button>
+            <button
+              onClick={() => {
+                setNewEntry(true);
+                setEdit(null);
+              }}
+            >
+              +
+            </button>
           </td>
         </tr>
       ) : (
         <tr>
           <td>
-            <select
-              type="dropdown"
-              onChange={(e) => setSubCategory(e.target.value)}
-              name="sub_category"
-            >
-              <option value="none" selected disabled hidden></option>
+            {!newCategory ? (
+              <select
+                type="dropdown"
+                onChange={(e) => {
+                  setSubCategory(e.target.value);
+                  if (e.target.value === "new") {
+                    setNewCategory(!newCategory);
+                    setSubCategory("");
+                  }
+                }}
+                name="sub_category"
+              >
+                <option value="none" selected disabled hidden>
+                  Select Category
+                </option>
 
-              {renderOptions}
+                <option value="new">New Category</option>
 
-              <option>+ ...</option>
-            </select>
+                {renderOptions}
+              </select>
+            ) : (
+              <input
+                type="text"
+                onChange={(e) => setSubCategory(e.target.value)}
+                name="subCategory"
+                value={subCategory}
+              />
+            )}
           </td>
           <td>
             <input
