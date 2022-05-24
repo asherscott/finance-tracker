b9b79575b6f8db6fc46ee9fed8d0a6bae90f8ffc
asher scott <asher.scott6@gmail.com>
2022-05-24 14:46:28 -0500

    cashflow crud rendering properly

--- client/src/CashFlow.js
+++ client/src/CashFlow.js
@@ -1,9 +1,16 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import RenderRows from "./RenderRows";
 
 function CashFlow({ user }) {
   const [canEdit, setCanEdit] = useState(false);
   const [categoryId, setCategoryId] = useState(null);
+  const [tab, setTab] = useState(null);
+  const [filterEntries, setFilterEntries] = useState(
+    user.journal_entries.filter(
+      (entry) =>
+        entry.category.name === "Expense" || entry.category.name === "Income"
+    )
+  );
   const [entries, setEntries] = useState(
     user.journal_entries.filter(
       (entry) =>
@@ -11,21 +18,21 @@ function CashFlow({ user }) {
     )
   );
 
+  useEffect(() => {
+    setFilterEntries(entries.filter((entry) => entry.category.name === tab));
+  }, [entries]);
+
   function handleTab(category) {
+    setTab(category);
+
     if (category) {
-      setEntries(
-        user.journal_entries.filter((entry) => entry.category.name === category)
+      setFilterEntries(
+        entries.filter((entry) => entry.category.name === category)
       );
       setCanEdit(true);
       setCategoryId(category === "Income" ? 1 : 2);
     } else {
-      setEntries(
-        user.journal_entries.filter(
-          (entry) =>
-            entry.category.name === "Expense" ||
-            entry.category.name === "Income"
-        )
-      );
+      setFilterEntries(entries);
       setCanEdit(false);
     }
   }
@@ -46,7 +53,7 @@ function CashFlow({ user }) {
           </tr>
         </thead>
         <RenderRows
-          journalEntries={entries}
+          journalEntries={filterEntries}
           setJournalEntries={setEntries}
           user={user}
           hasDate={true}
